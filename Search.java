d6d9623d940e62715eedd17be6a08afff9067794
AnNguyenVan123 <131338516+AnNguyenVan123@users.noreply.github.com>
2024-03-21 20:11:47 +0700

    Delete Search.java

--- Search.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-
-public interface Search {
-    public ArrayList<Product>search( ArrayList<Product> list_product, String key);
-    public ArrayList<Product>search( ArrayList<Product> list_product, int min , int max);
-}
