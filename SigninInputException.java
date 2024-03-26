daec2c894dd6e69826c6f70a07dc75627623c5ff
AnNguyenVan123 <131338516+AnNguyenVan123@users.noreply.github.com>
2024-03-26 19:42:18 +0700

    Delete SigninInputException.java

--- SigninInputException.java
+++
@@ -1,9 +0,0 @@
-public class SigninInputException implements InputException{
-    @Override
-    public Exception getException(int input) {
-        if(input <1 || input>2){
-            return new Exception("Lua chon ko hop le");
-        };
-        return null;
-    }
-}
