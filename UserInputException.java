ccd595332f422404ebc7b02d99dddeb461652ccb
AnNguyenVan123 <131338516+AnNguyenVan123@users.noreply.github.com>
2024-03-26 19:43:25 +0700

    Delete UserInputException.java

--- UserInputException.java
+++
@@ -1,9 +0,0 @@
-public class UserInputException implements  InputException{
-    @Override
-    public Exception getException(int input) {
-        if(input <1 || input>4){
-            return new Exception("Lua chon ko hop le");
-        };
-        return null;
-    }
-}
